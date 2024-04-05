499b2badb940c1c47504cfe370418a332d3113f4
chompy23 <sdefederico1@gmail.com>
2024-04-05 16:41:06 -0300

    Segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("¡Concluí la Aula 01 y ahora estoy sumergiendome mucho en en java!");
+        System.out.println("Bienvenido(a) a Screen Match");
+        System.out.println("Película: Matrix");
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.8 + 9.0) / 3;
+        System.out.println(media);
+        String sinopsis = """
+                Matrix es una paradoja.
+                La mejór película del milenio.
+                Fue lanzada en :
+                """ + fechaDeLanzamiento;
+        System.out.println(sinopsis);
+
+        int clasificacion = (int) (media / 2);
+        System.out.println(clasificacion);
+
     }
 }
