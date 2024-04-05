a7944316c851bcb79768a470ce5ba668884e9c29
chompy23 <sdefederico1@gmail.com>
2024-04-05 16:57:01 -0300

    Primer commit

---
+++ src/Desiciones.java
@@ -0,0 +1,14 @@
+public class Desiciones {
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        if (fechaDeLanzamiento > 2022){
+            System.out.println("Películas mas Populares");
+        }else {
+            System.out.println("Películas Retro que aun vale la Pena Ver");
+        }
+    }
+}
+
